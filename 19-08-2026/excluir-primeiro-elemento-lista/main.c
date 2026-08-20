3c9f419e5b3cfc52849fbf5a289c5e161853be85
gabriel-baptista-0607 <gabriel.baptista@ogfigovernance.com>
2026-08-19 22:38:33 -0300

    remove first node from linked list

---
+++ 19-08-2026/excluir-primeiro-elemento-lista/main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no {
+  int info;
+  struct no *prox;
+} No;
+
+typedef struct lista {
+  No *inicio;
+} Lista;
+
+Lista* inicializarLista() {
+  return NULL;
+}
+
+Lista* criarLista() {
+  Lista *aux;
+  aux = (Lista *) malloc(sizeof(Lista));
+  aux->inicio = NULL;
+  return aux;
+}
+
+No* aux_insere(No *antigo, int v) {
+  No *aux;
+  aux = (No*) malloc(sizeof(No));
+  aux->info = v;
+  aux->prox = antigo;
+  return aux;
+}
+
+void insereL_inicio(Lista *velho, int valor) {
+  velho->inicio = aux_insere(velho->inicio, valor);
+}
+
+void imprimir_lista(Lista* l1) {
+  No *aux;
+  aux = l1->inicio;
+  printf("Itens da lista encadeada: ");
+  while (aux != NULL) {
+    printf("%d ", aux->info);
+    aux = aux->prox;
+  }
+  printf("\n");
+}
+
+void remover_primeiro_item(Lista* l1) {
+  No *aux;
+  No *prev_aux;
+  aux=l1->inicio;
+  if (l1->inicio == NULL) {
+    free(aux);
+    l1->inicio=NULL;
+    return;
+  }
+
+  if (l1->inicio != NULL && l1->inicio->prox == NULL) {
+    free(aux);
+    l1->inicio=NULL;
+    return;
+  }
+
+  while (aux->prox != NULL) {
+    prev_aux = aux;
+    aux = aux->prox;
+  }
+  prev_aux->prox = NULL;
+  free(aux);
+}
+
+int main() {
+  Lista *l;
+  l = inicializarLista();
+  l = criarLista();
+  insereL_inicio(l, 11);
+  insereL_inicio(l, 10);
+  insereL_inicio(l, 9);
+  imprimir_lista(l);
+  remover_primeiro_item(l);
+  imprimir_lista(l);
+
+  return 0;
+}
